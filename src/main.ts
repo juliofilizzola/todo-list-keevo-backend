4ff52b0e1613e4d3b8dc54500b0296111a1f96d9
Julio Filizzola <juliofilizzola@Hotmail.com>
2024-02-02 14:26:19 -0300

    feat: add enable cors and global pipes

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
   const port = process.env.PORT || 3000;
-  await app.listen(port).then(() => console.log(`project running in port: ${port}`));
+  await app
+    .listen(port)
+    .then(() => console.log(`project running in port: ${port}`));
 }
 bootstrap();
