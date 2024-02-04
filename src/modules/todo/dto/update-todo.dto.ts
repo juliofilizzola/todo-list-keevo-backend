961a57502907c5213cd5858d3f6924f104225a90
Julio Filizzola <juliofilizzola@Hotmail.com>
2024-02-04 11:42:00 -0300

    feat: add new parameter from update todo dto (status)

--- src/modules/todo/dto/update-todo.dto.ts
+++ src/modules/todo/dto/update-todo.dto.ts
@@ -1,4 +1,11 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateTodoDto } from './create-todo.dto';
+import { StatusTodo } from '@prisma/client';
+import { IsEnum } from 'class-validator';
 
-export class UpdateTodoDto extends PartialType(CreateTodoDto) {}
+export class UpdateTodoDto extends PartialType(CreateTodoDto) {
+  @IsEnum(StatusTodo, {
+    message: 'status invalid',
+  })
+  status: StatusTodo;
+}
