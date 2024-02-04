3cb8c366517b1bd611685fafbe6e8b59338d556e
Julio Filizzola <juliofilizzola@Hotmail.com>
2024-02-04 11:41:26 -0300

    feat: add class validator from dto create-todo

--- src/modules/todo/dto/create-todo.dto.ts
+++ src/modules/todo/dto/create-todo.dto.ts
@@ -1,4 +1,19 @@
+import { IsNotEmpty, IsString } from 'class-validator';
+
 export class CreateTodoDto {
+  @IsString({
+    message: 'title invalid',
+  })
+  @IsNotEmpty({
+    message: 'title is required',
+  })
   title: string;
+
+  @IsString({
+    message: 'description invalid',
+  })
+  @IsNotEmpty({
+    message: 'description is required',
+  })
   description: string;
 }
