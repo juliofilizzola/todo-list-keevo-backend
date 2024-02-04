b6bbc7bc5d184e7120913661a1259fa14a30ec28
Julio Filizzola <juliofilizzola@Hotmail.com>
2024-02-04 14:13:40 -0300

    feat: populate todo entity return todo

--- src/modules/todo/entities/todo.entity.ts
+++ src/modules/todo/entities/todo.entity.ts
@@ -1,1 +1,49 @@
-export class Todo {}
+import { ApiProperty } from '@nestjs/swagger';
+
+export class Todo {
+  @ApiProperty({
+    example: '14087e68-e669-482b-b908-bc33623a036d',
+    required: true,
+    description: 'id do usuario',
+    type: String,
+  })
+  id: string;
+
+  @ApiProperty({
+    example: 'comprar leite',
+    required: true,
+    description: 'Titulo da tarefa',
+    type: String,
+  })
+  title: string;
+
+  @ApiProperty({
+    example: 'preciso de leite para fazer bolo',
+    required: true,
+    description: 'Descrição da tarefa',
+    type: String,
+  })
+  description: string;
+  @ApiProperty({
+    required: true,
+    type: Date,
+    example: new Date(),
+    description: 'campo retorna quando a todo foi criada',
+  })
+  createdAt: Date;
+  @ApiProperty({
+    required: true,
+    type: Date,
+    example: new Date(),
+    description: 'campo retorna a ultima atualização',
+  })
+  deletedAt: Date;
+
+  @ApiProperty({
+    required: true,
+    type: Date,
+    example: new Date(),
+    description: 'campo retorna a ultima atualização',
+  })
+  updatedAt: Date;
+}
